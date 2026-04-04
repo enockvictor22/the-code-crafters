ad7627da7deb935883ad275e8df5c2a930d47dec
venock <enockvictor2022@gmail.com>
2026-04-04 04:50:55 +0100

    calculator

--- thecodecrafterthon-day1/calc.go
+++ thecodecrafterthon-day1/calc.go
@@ -6,7 +6,7 @@ import "fmt"
 // "strings"
 
 func add(a, b int) int {
-	return a+b
+	return a + b
 }
 
 func subtract(a, b int) int {
@@ -14,27 +14,27 @@ func subtract(a, b int) int {
 }
 
 func divide(a, b int) int {
-	return a/b
+	return a / b
 }
 
 func multiply(a, b int) int {
-	return a*b
+	return a * b
 }
 
 func main() {
 	fmt.Println("Welcome CodeCrafter, this is a CLI calculator")
 	for {
-	fmt.Println("Choose an operation from the options below:")
+		fmt.Println("Choose an operation from the options below:")
 
-	var opt int
-	fmt.Println(" [1] addition\n [2] subtraction\n [3] Division\n [4] multiplication\n [5] Help\n [6] Quit")
-	fmt.Scan(&opt)
+		var opt int
+		fmt.Println(" [1] addition\n [2] subtraction\n [3] Division\n [4] multiplication\n [5] Help\n [6] Quit")
+		fmt.Scan(&opt)
 
-	if opt <0 || opt > 6 {
-		fmt.Println("invalid operation. Select [5] help for guidance")
-		continue
-		
+		if opt < 0 || opt > 6 {
+			fmt.Println("invalid operation. Select [5] help for guidance")
+			continue
+
+		}
+
 	}
-
-}
 }
